ab6c3af99420364b3482b11a0cb5bd182c4ef35c
Charmoni Pi <120690178+GitMon1@users.noreply.github.com>
2023-09-18 02:52:59 +0000

    1054

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-posts = [
-   {
-        'author': 'Charmoni Pi',
-        'title': 'The Good Gil',
-        'content': 'This is Alot',
-        'date_posted': 'August 27, 2023',
-   }, 
-      {
-        'author': 'Charmoni Pi2',
-        'title': 'The Good Gil22',
-        'content': 'This is Alo2222t',
-        'date_posted': 'August 27, 2023',
-   } 
-]
 
 # Create your views here.
 def home(request):
     context ={
-        'posts': posts    
+        'posts': Post.objects.all()    
     }
     return render(request, 'blog/home.html', context)
 
